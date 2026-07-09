894ad2e5c7733ea6b6ce9ab54f59ab678fd5d595
jackie020105-beep <jackie020105@gmail.com>
2026-07-09 10:30:54 +0900

    Add files via upload

---
+++ day05/extend02/Employee.java
@@ -0,0 +1,27 @@
+package kr.ac.kopo.day05.extend02;
+
+public class Employee {
+	/** 사원번호 */
+	int no;
+	/** 사원이름 */
+	String name;
+	/** 사원직급 */
+	String grade;
+	/** 사원연봉 */
+	int salary;
+	
+	public Employee() {
+		
+	}
+	
+	Employee(int no, String name, String grade, int salary){
+		this.no = no;
+		this.name = name;
+		this.grade = grade;
+		this.salary = salary;
+	}
+	void showInfo() {
+		System.out.println("사원번호 : " + no + ", 이름 : " + name + ", 직급 : " + grade + ", 연봉 : " +  salary + "만원");
+	}
+	
+}
